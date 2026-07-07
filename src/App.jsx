fc7544fb523aedf176aaeff57409f1704698b86c
Asilbek <tdmlider00@gmail.com>
2026-07-07 08:58:08 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
 import React from 'react'
-import Navbar from './components/Omatillo1/Navbar'
-import Kardlar from './components/Samandar2/Kardlar'
 
 const App = () => {
   return (
-    <div>
-
-      <Navbar />
-      <Kardlar />
-
-    </div>
+    <div>App</div>
   )
 }
 
